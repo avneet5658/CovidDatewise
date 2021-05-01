d8027f14c3344b0b9949a657ad6e78e8a534fe06
avneet5658 <avneet.pandey@mail.vinove.com>
2021-05-01 10:22:58 +0530

    initial Commit

---
+++ src/component/ConfirmedGlobal.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { csv } from "d3";
+import { CChart } from "@coreui/react-chartjs";
+const ConfirmedGlobal = () => {
+  const [covidData, setCovidData] = useState("");
+  const [country, setCountry] = useState("");
+
+  const changeState = (tempData, state, setState) => {
+    let tempState = state;
+    tempState.push(tempData);
+    setState(tempState);
+  };
+
+  useEffect(() => {
+    var tempCountry = "";
+    csv(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-31-2020.csv"
+    )
+      .then((data) => {
+        console.log(data);
+        setCovidData(data);
+        tempCountry = new Set(data.map((d) => d.Country_Region));
+        setCountry([...tempCountry]);
+      })
+      .catch((err) => console.log(err));
+    //     csv(
+    //       "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    //     )
+    //       .then((data) => console.log(data))
+    //       .catch((err) => console.log(err));
+    //
+  }, []);
+
+  const handleChange = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <div>
+      <select onChange={(e) => handleChange(e)}>
+        {country &&
+          country.map((countryName, index) => (
+            <option key={index} value={countryName}>
+              {countryName}
+            </option>
+          ))}
+      </select>
+
+      <CChart
+        type="bar"
+        datasets={[
+          {
+            label: "Confirmed",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            tooltipLabelColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56, 55, 40],
+          },
+          {
+            label: "Deaths",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            tooltipLabelColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40, 19, 96, 27, 100],
+          },
+        ]}
+        options={{
+          aspectRatio: 1.5,
+          tooltips: {
+            enabled: true,
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default ConfirmedGlobal;
---
+++ src/component/Home.js
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { csv, index } from "d3";
+import { CChart } from "@coreui/react-chartjs";
+const Home = () => {
+  const [covidData, setCovidData] = useState("");
+  const [country, setCountry] = useState("");
+  const [filteredCountry, setFilteredCountry] = useState([]);
+  const [covidUpdate, setCovidUpdate] = useState({
+    provincedStates: [],
+    confirmedCases: [],
+    deaths: [],
+    recovered: [],
+    actives: [],
+  });
+  const initialState = {
+    provincedState: "",
+    confirmedCase: "",
+    death: "",
+    recover: "",
+    active: "",
+  };
+  const [stateUpdate, setStateUpdate] = useState(initialState);
+
+  useEffect(() => {
+    var tempCountry = "";
+    csv(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-31-2020.csv"
+    )
+      .then((data) => {
+        setCovidData(data);
+        tempCountry = new Set(data.map((d) => d.Country_Region));
+        setCountry([...tempCountry]);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleCountry = (e) => {
+    let countrySelected = e.target.value;
+    let tempData = covidData.filter(
+      (data) => data.Country_Region === countrySelected
+    );
+    setFilteredCountry(tempData);
+    setStateUpdate(initialState);
+    setCovidUpdate({
+      provincedStates: tempData.map((data) => data.Province_State),
+      confirmedCases: tempData.map((data) => data.Confirmed),
+      deaths: tempData.map((data) => data.Deaths),
+      recovered: tempData.map((data) => data.Recovered),
+      actives: tempData.map((data) => data.Active),
+    });
+  };
+  const handleProvincedStates = (e) => {
+    const index = covidUpdate.provincedStates.indexOf(e.target.value);
+
+    setStateUpdate({
+      provincedState: [e.target.value],
+      confirmedCase: covidUpdate.confirmedCases[index],
+      death: covidUpdate.deaths[index],
+      recover: covidUpdate.recovered[index],
+      active: covidUpdate.actives[index],
+    });
+  };
+
+  return (
+    <div>
+      <select onChange={(e) => handleCountry(e)}>
+        {country &&
+          country.map((countryName, index) => (
+            <option key={index} value={countryName}>
+              {countryName}
+            </option>
+          ))}
+      </select>
+
+      {covidUpdate.provincedStates.length > 1 && (
+        <select onChange={(e) => handleProvincedStates(e)}>
+          {covidUpdate.provincedStates.map((stateName, index) => (
+            <option key={index} value={stateName}>
+              {stateName}
+            </option>
+          ))}
+        </select>
+      )}
+
+      <div style={{ width: "70%", height: "80%" }}>
+        {filteredCountry.length > 0 ? (
+          <CChart
+            type="bar"
+            datasets={[
+              {
+                label: "Confirmed",
+                backgroundColor: "blue",
+                borderColor: "rgba(179,181,198,1)",
+                tooltipLabelColor: "rgba(179,181,198,1)",
+
+                // data: [...confirmedCases],
+                data:
+                  stateUpdate.provincedState.length > 0
+                    ? [stateUpdate.confirmedCase]
+                    : [...covidUpdate.confirmedCases],
+              },
+
+              {
+                label: "Actives",
+                backgroundColor: "yellow",
+                borderColor: "rgba(255,99,132,1)",
+                tooltipLabelColor: "rgba(255,99,132,1)",
+                // data: [...actives],
+                data:
+                  stateUpdate.provincedState.length > 0
+                    ? [stateUpdate.active]
+                    : [...covidUpdate.actives],
+              },
+              {
+                label: "Recovered",
+                backgroundColor: "green",
+                borderColor: "rgba(255,99,132,1)",
+                tooltipLabelColor: "rgba(255,99,132,1)",
+                // data: [...recovered],
+                data:
+                  stateUpdate.provincedState.length > 0
+                    ? [stateUpdate.recover]
+                    : [...covidUpdate.recovered],
+              },
+              {
+                label: "Deaths",
+                backgroundColor: "red",
+                borderColor: "rgba(255,99,132,1)",
+                tooltipLabelColor: "rgba(255,99,132,1)",
+                // data: [...deaths],
+                data:
+                  stateUpdate.provincedState.length > 0
+                    ? [stateUpdate.death]
+                    : [...covidUpdate.deaths],
+              },
+            ]}
+            options={{
+              aspectRatio: 1.5,
+              tooltips: {
+                enabled: true,
+              },
+            }}
+            labels={
+              stateUpdate.provincedState.length > 0
+                ? [stateUpdate.provincedState]
+                : [...covidUpdate.provincedStates]
+            }
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
